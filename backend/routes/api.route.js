b298e83146e43824a4b3864ec92206757bcfcc72
Smrutiranjan Patra <76831153+Smrutiranjan-Patra@users.noreply.github.com>
2022-04-01 02:54:12 +0530

    update

--- backend/routes/api.route.js
+++ backend/routes/api.route.js
@@ -1,7 +1,18 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { TwitterApi } = require("twitter-api-v2");
+const client = new TwitterApi({
+  appkey: process.env.API_KEY,
+  appSecret: process.env.API_KEY_SECRET,
+  accessToken: process.env.ACCESS_TOKEN_KEY,
+  accessSecret: process.env.ACCESS_TOKEN_SECRET,
+});
 
-router.get('/', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
+router.get("/post", async (req, res, next) => {
+  try {
+    res.status(200).send("user");
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
 });
 
 module.exports = router;
